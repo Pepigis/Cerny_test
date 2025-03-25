b76e9df78b93d7bb13c88dcd1a72b8f9315f4536
Pepigis <cernyjosef093@gmail.com>
2025-03-25 08:55:30 +0100

    oprava chyby

--- code-hard.js
+++ code-hard.js
@@ -4,7 +4,7 @@
 // funkce vrátí pole string hodnot kde každá má délku maxLength, které vychází z arrayInput a bude obsahovat pouze stringy s délkou v rozmezí minLength (včetně) až maxLength (včetně), delší nebo kratší budou přeskočeny, ty co byly v rozmezí budou doplněny pomocí symbolToFill na délku maxLength,
 // v případě prázdného nebo nedefinovaného arrayInput a nebo pokud požadavkům na délku nepdovídají žádné hodnoty, tak bude vráceno prázdné pole
 // například pro ["apple", "banana", "kiwi", "orange", "strawberry"], 5, 6, bude výsledek ["appleX", "banana", "orange"]
-function processArrayItems(arrayInput, minLength, maxLength, symbolToFill) {
+function processArrayItems(arrayInput, minLength, maxLength, symbolToFill = 'X') {
   if (!Array.isArray(arrayInput) || arrayInput.length === 0) {
     return [];
   }
